11c533057f6e5e5ee6aacb4394e197dba4ff11c1
fimac <fimccawley@gmail.com>
2017-04-30 18:00:46 +1000

    created engineers controller, with index, show, new, edit pages

---
+++ OdioApp/app/controllers/engineers_controller.rb
@@ -0,0 +1,13 @@
+class EngineersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- OdioApp/config/routes.rb
+++ OdioApp/config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'engineers/index'
+
+  get 'engineers/show'
+
+  get 'engineers/new'
+
+  get 'engineers/edit'
+
 get "/" => "pages#home"
 
 get '/about' => "pages#about"
+
+resources :engineers
 
 end
