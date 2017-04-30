617de646761df0b61de592ef47518b7f32de4a8b
fimac <fimccawley@gmail.com>
2017-04-30 18:49:09 +1000

    new, update and delete added. Basic CRUD for engineers completed

--- OdioApp/app/controllers/engineers_controller.rb
+++ OdioApp/app/controllers/engineers_controller.rb
@@ -4,20 +4,36 @@ class EngineersController < ApplicationController
   end
 
   def show
-    @engineer = Engineer.find_by(id: params['id'])
+    @engineer = Engineer.find_by( id: params['id'] )
   end
 
   def new
+    @engineer = Engineer.new
   end
 
   def create
-
+    engineer = Engineer.create ( engineer_strong_params )
+    redirect_to "/engineers/#{engineer.id}"
   end
 
   def edit
+    @engineer = Engineer.find_by( id: params['id'] )
   end
 
   def update
+    engineer = Engineer.find_by( id: params['id'] )
+    engineer.update ( engineer_strong_params )
+    redirect_to "/engineers/#{engineer.id}"
+  end
 
+  def destroy
+    engineer = Engineer.find_by( id: params['id'] )
+    engineer.destroy
+    redirect_to "/engineers"
+  end
+
+  private
+  def engineer_strong_params
+    params.require(:engineer).permit(:name, :bio, :image)
   end
 end
