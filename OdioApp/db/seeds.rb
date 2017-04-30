afed52f92e3b1960932641cebc8740d0248a1f12
fimac <fimccawley@gmail.com>
2017-04-30 19:02:19 +1000

    added dummy data and seeded artist table

--- OdioApp/db/seeds.rb
+++ OdioApp/db/seeds.rb
@@ -4,4 +4,12 @@ steve = Engineer.create(name: "Steve Albini", bio: "Owner and engineer at Electr
 flood = Engineer.create(name: "Flood", bio: "Co Owner and engineer at Assault & Battery 1, London")
 rick = Engineer.create(name: "Rick Rubin", bio: "The only producer you need")
 
-p = "Engineer count is #{Engineer.all.count}"
+p "Engineer count is #{Engineer.all.count}"
+
+Artist.destroy_all
+
+hashrocket = Artist.create(name: "Hash Rocket", bio: "Founded by Ginger in 2004, their first album Cat equals God was released in 2005 to critical acclaim. One of the few bands to overcome the cat dog barrier that often breaks bands apart, Hash Rocket continue to pioneer species diversity.", image: "http://static.boredpanda.com/blog/wp-content/uploads/2017/02/animals-about-to-drop-album-photos-31-58aee2dd27168__700.jpg")
+grooming = Artist.create(name: "Grooming", bio: "Escaping from a farm back in 2004, Grooming released their hit single Meat Bad in 2005. This led to their hit album, Fur Feather Love in 2006.", image: "http://static.boredpanda.com/blog/wp-content/uploads/2017/02/animals-about-to-drop-album-photos-58aeb2b30934d__700.jpg")
+rails = Artist.create(name: "Rails", bio: "They met each other through their love affair of acorns. The first album Oh Squirrel, my squirrel, hit #3 on the billboard music chart in 2006. Riding high on this, they released their unfortunately titled follow up, Hiding My Nuts in 2007, which was universally panned.", image: "They met each other through their love affair of acorns. The first album Oh Squirrel, my squirrel, hit #3 on the billboard music chart in 2006. Riding high on this, they released their unfortunately titled follow up, Hiding My Nuts in 2007, which was universally panned.")
+
+p "Artist count is #{Artist.all.count}"
