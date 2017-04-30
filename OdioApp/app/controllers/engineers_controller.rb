a63313a992bb69bfe67bcf6f680402256b624c5b
fimac <fimccawley@gmail.com>
2017-04-30 18:19:39 +1000

    index page showing list of engineers, and show page showing engineer info.

--- OdioApp/app/controllers/engineers_controller.rb
+++ OdioApp/app/controllers/engineers_controller.rb
@@ -1,13 +1,23 @@
 class EngineersController < ApplicationController
   def index
+    @all_engineers = Engineer.all
   end
 
   def show
+    @engineer = Engineer.find_by(id: params['id'])
   end
 
   def new
   end
 
+  def create
+
+  end
+
   def edit
+  end
+
+  def update
+
   end
 end
--- OdioApp/config/routes.rb
+++ OdioApp/config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'engineers/index'
-
-  get 'engineers/show'
-
-  get 'engineers/new'
-
-  get 'engineers/edit'
-
 get "/" => "pages#home"
 
 get '/about' => "pages#about"
