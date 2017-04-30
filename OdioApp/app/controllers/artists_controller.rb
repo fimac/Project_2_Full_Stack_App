ca967d76fd33db18d1eff0b6d5e237b9a841e3d9
fimac <fimccawley@gmail.com>
2017-04-30 20:48:48 +1000

    crud completed for artists

--- OdioApp/app/controllers/artists_controller.rb
+++ OdioApp/app/controllers/artists_controller.rb
@@ -4,19 +4,37 @@ class ArtistsController < ApplicationController
   end
 
   def show
+    @artist = Artist.find_by( id: params['id'] )
   end
 
   def new
+    @artist = Artist.new
   end
 
   def create
-
+    artist = Artist.create( artist_strong_params )
+    redirect_to "/artists/#{artist.id}"
   end
 
   def edit
+    @artist = Artist.find_by(id: params['id'])
   end
 
   def update
+    artist = Artist.find_by(id: params['id'])
+    artist.update ( artist_strong_params )
+    redirect_to "/artists/#{artist.id}"
+  end
+
+  def destroy
+    artist = Artist.find_by(id: params['id'])
+    artist.destroy
+    redirect_to "/artists"
+  end
+
+  private
+  def artist_strong_params
+    params.require(:artist).permit(:name, :bio, :image)
 
   end
 end
